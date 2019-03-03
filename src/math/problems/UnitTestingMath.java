8cad0e0e6e83362410bcb045c2343b9fd129b179
Shameem77 <shameembusa@gmail.com>
2019-03-03 18:52:33 -0500

    unit testing

--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -6,7 +6,7 @@ public class UnitTestingMath {
     public static void main(String[] args) {
         //Apply Unit testing into each classes and methods in this package.
         try {
-            Assert.assertEquals(5, 110);
+            Assert.assertEquals(5, 10);
             System.out.println("Unit testing passed");
         }catch (AssertionError ar){
             System.out.println("Unit testing failed");
