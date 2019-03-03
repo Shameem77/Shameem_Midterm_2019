44bb55303de3fa89624309340b0f99c8d7ded50d
Shameem77 <shameembusa@gmail.com>
2019-03-02 19:36:11 -0500

    Fibonacci

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -7,6 +7,15 @@ public class Fibonacci {
           0,1,1,2,3,5,8,13
          */
 
+        int n, a = 0, b = 0, c = 1;
+        System.out.print("40 Fibonacci numbers: ");
+        for(int i = 1; i <= 40; i++) {
+            a = b;
+            b = c;
+            c = a + b;
+            System.out.print(a + ", ");
+        }
 
     }
 }
+
