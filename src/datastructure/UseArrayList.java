36d5cbdb88c092d70c715fa6976786fad8cbe47b
Shameem77 <shameembusa@gmail.com>
2019-03-08 23:18:25 -0500

    use array list

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,4 +1,10 @@
 package datastructure;
+import databases.ConnectToSqlDB;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 public class UseArrayList {
 
@@ -9,6 +15,36 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
+		List<String> name = new ArrayList<>();
+
+		name.add("Shameem");
+		name.add("Shaheen");
+		name.add("Masuma");
+		name.add("Mahmuda");
+		name.add("Mafi");
+		name.add("Rakib");
+
+		System.out.println("Retrieving data from ArrayList: " + name);
+		System.out.println("Peek element: " + name.get(2));
+		System.out.println("Remove element at index 4: " + name.remove(4));
+		System.out.println("After doing all these, retrieving data from ArrayList: " + name);
+
+		System.out.print("By using for Loop: ");
+		for (String st : name) {
+			System.out.print(" "+st);
+		}
+		System.out.println();
+
+		System.out.print("By using while Loop with Iterator: ");
+		Iterator it = name.iterator();
+		while (it.hasNext()) {
+			String s = (String) it.next();
+			System.out.print(" "+s);
+		}
+		System.out.println();
+
+		Collections.sort(name);
+		System.out.println("After sorting, the ArrayList: "+name);
 	
 
 	}
