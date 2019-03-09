fc9920e24baa1166381b67c5d99096a9c7e8410e
Shameem77 <shameembusa@gmail.com>
2019-03-08 22:38:39 -0500

    data reader

--- src/datastructure/DataReader.java
+++ src/datastructure/DataReader.java
@@ -1,4 +1,15 @@
 package datastructure;
+import databases.ConnectToSqlDB;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 
 public class DataReader {
 
@@ -19,6 +30,76 @@ public class DataReader {
 		 */
 
 		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
+		System.out.println(textFile);
+		Stack<String> stack = new Stack<String>();
+
+		LinkedList<String> list = new LinkedList<String>();
+
+		BufferedReader br = null;
+
+		String fileText = null;
+
+		try {
+
+			FileReader fr = new FileReader(textFile);
+
+			br = new BufferedReader(fr);
+
+		}catch (FileNotFoundException e) {
+
+			System.out.println(e);
+
+		}
+
+		try {
+
+			if (br.ready())
+
+				fileText = br.readLine();
+
+		}catch (IOException e) {
+
+			System.out.println(e);
+
+		}
+
+		catch (NullPointerException e)
+
+		{
+
+			System.out.println(e);
+
+		}
+
+		String [] words = fileText.split(" ");
+
+		for(int i=0;i<words.length;i++) {
+
+			System.out.println(words[i]);
+
+			stack.push(words[i]);
+
+			list.add(words[i]);
+
+		}
+
+		System.out.println("Stack Peek:  "+  stack.peek());
+
+		System.out.println("Stack Pop:  "+  stack.pop());
+
+		System.out.println("Stack Search:  "+stack.search("Google"));
+
+
+
+		Iterator it = list.iterator();
+
+		System.out.println("Linked List Display:");
+
+		while (it.hasNext())
+
+			System.out.println(it.next());
+
+
 
 
 
