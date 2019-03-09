cd18053dfb4335a7c41c8c345688b2ec176691b7
Shameem77 <shameembusa@gmail.com>
2019-03-08 23:46:30 -0500

    use queue

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,5 +1,9 @@
 package datastructure;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UseQueue {
 
 	public static void main(String[] args) {
@@ -8,6 +12,29 @@ public class UseQueue {
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 * 
 		 */
+
+		Queue<String> queue = new LinkedList<String>();
+		queue.add("Shameem");  //head, first
+		queue.add("Shaheen");
+		queue.add("Romel");
+		queue.add("Mafi");
+		queue.add("Tofael");  //tail, last
+		System.out.println("\nFirst element of the queue: "+queue.peek()+"\n"); //Retrieves, but does not remove, the head of this queue, or returns null if this queue is empty.
+		queue.remove();    //Retrieves and removes the head of this queue.
+		System.out.println("After removing head (by remove method), first element of this queue is: "+queue.peek());
+
+		System.out.println("\nPrinting queue using for each loop: ");
+		for(String st : queue) {
+			System.out.print(st + " ");
+		}
+		queue.add("Nishat");
+		queue.poll();    //Retrieves and removes the head of this queue, or returns null if this queue is empty.
+
+		System.out.println("\n\nPrinting queue After adding and removing one element using Iterator ");
+		Iterator it = queue.iterator();
+		while(it.hasNext()){
+			System.out.print(it.next()+" ");
+		}
 
 	}
 
